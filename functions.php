a627f7f86b48b2968b632b14e155fd077b2527c0
Dmitry Lyapin <lyapindm@yandex.ru>
2020-03-12 16:09:20 +0300

    Хедер

---
+++ functions.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * aquaphor functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package aquaphor
+ */
+
+
+if( ! defined('AQUAPHOR_THEME_VERSION') )  define('AQUAPHOR_THEME_VERSION', wp_get_theme()->get( 'Version' ) ); 
+if( ! defined('AQUAPHOR_THEME_PATH') )     define('AQUAPHOR_THEME_PATH', get_template_directory() ); 
+if( ! defined('AQUAPHOR_THEME_URL') )      define('AQUAPHOR_THEME_URL', get_template_directory_uri() ); 
+if( ! defined('AQUAPHOR_THEME_ASSETS') )   define('AQUAPHOR_THEME_ASSETS', get_template_directory_uri().'/assets' ); 
+
+/**
+ *  Отключаем блоки в header
+ * 
+ * 
+ */
+
+
+
+if ( ! function_exists( 'storefront_product_search' ) ) {
+    function storefront_product_search() {
+        if ( storefront_is_woocommerce_activated() ) {
+           ?>
+           <?php
+        }
+    }
+}
+
+if ( ! function_exists( 'storefront_site_branding' ) ) {
+	function storefront_site_branding() {
+		?>
+    <?php
+	}
+}
+
+if ( ! function_exists( 'storefront_primary_navigation' ) ) {
+	/**
+	 * Display Primary Navigation
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	function storefront_primary_navigation() {
+		?>
+		<?php
+	}
+}
+
+if ( ! function_exists( 'storefront_secondary_navigation' ) ) {
+	/**
+	 * Display Secondary Navigation
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	function storefront_secondary_navigation() {
+		if ( has_nav_menu( 'secondary' ) ) {
+			?>
+			<?php
+		}
+	}
+}
+
+if ( ! function_exists( 'storefront_header_cart' ) ) {
+	/**
+	 * Display Header Cart
+	 *
+	 * @since  1.0.0
+	 * @uses  storefront_is_woocommerce_activated() check if WooCommerce is activated
+	 * @return void
+	 */
+	function storefront_header_cart() {
+	}
+}
+
+
+/**
+ *  Регистрируем виджеты в header
+ */
+
+register_sidebar( array(
+   'name'          => 'Header Top Widget',
+   'id'            => 'header-top-widget',
+   'class'         => 'header-top-widget',
+   'before_widget' => '<div class="header-top-widget">',
+   'after_widget'  => '</div>'
+) );
+
+register_sidebar( array(
+    'name'          => 'Header Widget',
+    'id'            => 'header-widget',
+    'class'         => 'header-widget',
+    'before_widget' => '<div class="header-widget">',
+    'after_widget'  => '</div>'
+ ) );
+ 
+/**
+ *  Грузим наши стили
+ */
+
+/**
+ *  Грузим скрипт в в HEAD сайта
+ */
+function aquaphor_enqueue_styles() {
+
+	$parent_style = 'parent-style'; // This is 'StoreFront - style' for the StoreFront theme.
+
+ wp_enqueue_style( $parent_style, AQUAPHOR_THEME_URL . '/style.css' );
+	wp_enqueue_style( 'child-style',
+			get_stylesheet_directory_uri() . '/style.css',
+			array( $parent_style ),
+			wp_get_theme()->get('Version')
+	);
+}
+
+
+function aquaphor_wp_head(){
+	?>
+	<script>
+	let  a = 6;
+	let  b = 8;
+	let  m = a + b;
+
+	</script>
+
+	<?php
+}
+
+add_action( 'wp_head', 'aquaphor_wp_head');
