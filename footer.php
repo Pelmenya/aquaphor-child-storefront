c0e7d5962552e72b014c257524a630355ede8591
Dmitry Lyapin <lyapindm@yandex.ru>
2020-03-16 15:43:25 +0300

    футер с иконками

--- footer.php
+++ footer.php
@@ -14,17 +14,17 @@
 
 	<?php do_action( 'storefront_before_footer' ); ?>
 
-	<footer class="footer" role="contentinfo">
+	<footer class="footer">
   <div class="footer__container">
     <div class="footer__wrap">
-      <div class="footer__info">
+    <div class="footer__info">
         <img class="footer__logo" src="http://aquaphor.store/wp-content/uploads/2020/03/logo_white.png" alt="Логотип Аквафор">
-        <p class="footer__copyright">ООО «Аквафор Инжиниринг» &copy; <?php  echo  absint( date('Y' ) ); ?></p>
+        <p class="footer__copyright">ООО&nbsp;«Аквафор&nbsp;Инжиниринг»&nbsp;&copy;&nbsp;<?php  echo  absint( date('Y' ) ); ?></p>
         <div class="footer__cocial">
-         <i class="footer__cocial-link"></i>
-         <i class="footer__cocial-link"></i>
-         <i class="footer__cocial-link"></i>
-         <i class="footer__cocial-link"></i>
+         <i class="footer__cocial-link footer__cocial-link_vk"></i>
+         <i class="footer__cocial-link footer__cocial-link_ok"></i>
+         <i class="footer__cocial-link footer__cocial-link_fb"></i>
+         <i class="footer__cocial-link footer__cocial-link_in"></i>
         </div>
       </div>
       <div class="footer__columns">
@@ -49,15 +49,33 @@
       </div>
       <div class="footer__columns">
         <div class="footer__col">
-            <h4 class="footer__col-title">Системы</h4>
-            <a class="footer__col-link" href="#">WaterBoss</a>
-            <a class="footer__col-link" href="#">WaterMax</a>
-            <a class="footer__col-link" href="#">Аквафор</a>
-            <a class="footer__col-link" href="#">Aquaphor Pro</a>
-            <a class="footer__col-link" href="#">Колонны</a>
+            <h4 class="footer__col-title">Обратный&nbsp;осмос</h4>
+            <a class="footer__col-link" href="#">DWM-101S&nbsp;«Морион»</a>
+            <a class="footer__col-link" href="#">DWM-201</a>
+            <a class="footer__col-link" href="#">Осмо-50</a>
+            <a class="footer__col-link" href="#">Осмо&nbsp;Классика</a>
+            <a class="footer__col-link" href="#">Осмо-Кристалл</a>
          </div>
       </div>
-
+      <div class="footer__columns">
+        <div class="footer__col">
+            <h4 class="footer__col-title">Услуги</h4>
+            <a class="footer__col-link" href="#">Анализ&nbsp;воды</a>
+            <a class="footer__col-link" href="#">Подбор&nbsp;оборудивания</a>
+            <a class="footer__col-link" href="#">Сервис&nbsp;и&nbsp;ремонт</a>
+            <a class="footer__col-link" href="#">Регистрация&nbsp;фильтра</a>
+            <a class="footer__col-link" href="#">Оплата&nbsp;и&nbsp;доставка</a>
+         </div>
+      </div>
+      <div class="footer__columns">
+        <div class="footer__col">
+            <h4 class="footer__col-title">Информация</h4>
+            <a class="footer__col-link" href="#">Договор&nbsp;оферты</a>
+            <a class="footer__col-link" href="#">Политика&nbsp;конфиденциальности</a>
+            <a class="footer__col-link" href="#">Пользовательское&nbsp;соглашение</a>
+            <a class="footer__col-link" href="#">Процесс&nbsp;передачи&nbsp;данных</a>
+         </div>
+      </div>
 
     </div>
   </div>
