94b79564f09a13c94d041026a1e1a653dcbe4a8e
Dmitry Lyapin <lyapindm@yandex.ru>
2020-03-18 16:53:27 +0300

    Учетная запись

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,31 @@
 function main() {
-  const userNameInput = document.getElementById('username');
-  const passwordInput = document.getElementById('password');
-  const regEmailInput = document.getElementById('reg_email');
-
+  /**
+   *  Форма логина
+   */
   const formLogin = document.querySelector('.login');
+  /**
+   *  Форма регистрации
+   */
   const formRegister = document.querySelector('.register');
+
+  formLogin.elements.username.placeholder = 'Введите @почту';
+  formLogin.elements.password.placeholder = 'Введите пароль';
+  formRegister.elements.email.placeholder = 'Введите @почту';
+
+  /** Кнопки */
+  formLogin.elements.login.style.backgroundColor = 'var(--blue-blue)';
+  formLogin.elements.login.style.color = '#fff';
+  formLogin.elements.login.style.width = '160px';
+  formLogin.elements.login.style.fontSize = '13px';
+  formLogin.elements.login.value = 'Далее';
+  formLogin.elements.login.textContent = 'Далее';
+
+  formRegister.elements.register.style.backgroundColor = 'var(--blue-blue)';
+  formRegister.elements.register.style.color = '#fff';
+  formRegister.elements.register.style.width = '160px';
+  formRegister.elements.register.style.fontSize = '13px';
+  formRegister.elements.register.value = 'Зарегистрироваться';
+  formRegister.elements.register.textContent = 'Зарегистрироваться';
 
   const arrayOfElementP = formRegister.querySelectorAll('p');
 
@@ -22,13 +43,6 @@ function main() {
   arrayOfElementP[2].style.fontFamily = 'Proxima Nova Rg';
   arrayOfElementP[2].style.color = 'var(--dark)';
   arrayOfElementP[2].style.marginBottom = '22px';
-
-
-  console.log(formLogin);
-
-  userNameInput.placeholder = 'Введите @почту';
-  passwordInput.placeholder = 'Введите пароль';
-  regEmailInput.placeholder = 'Введите @почту';
 }
 
 main();
