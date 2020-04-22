2b75b109c18d90041689164f325c7fb06d88eada
Dmitry Lyapin <lyapindm@yandex.ru>
2020-04-22 16:38:44 +0300

    сделал страницу каталога

--- assets/js/pages/product-category/index.js
+++ assets/js/pages/product-category/index.js
@@ -16,7 +16,7 @@ function createElementDOM(
   classElement,
   textContent = '',
   styleElement = '',
-  datetime = ''
+  datetime = '',
 ) {
   const newElement = document.createElement(element);
   newElement.className = classElement;
@@ -58,7 +58,7 @@ function main() {
     headerH1.style.color = '#223c52';
 
     const addToCartBtns = document.querySelectorAll(
-      '.button.product_type_simple.add_to_cart_button.ajax_add_to_cart'
+      '.button.product_type_simple.add_to_cart_button.ajax_add_to_cart',
     );
     if (addToCartBtns) {
       Object.keys(addToCartBtns).forEach((i) => {
