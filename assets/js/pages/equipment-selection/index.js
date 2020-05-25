cf1fc6cbc1fb527ba1d4b93fec5d3b9633424541
Dmitry Lyapin <lyapindm@yandex.ru>
2020-05-25 16:43:29 +0300

    Сделал верстку систем, ситема не найдена

--- assets/js/pages/equipment-selection/index.js
+++ assets/js/pages/equipment-selection/index.js
@@ -22,10 +22,16 @@ function main() {
   fetch(req).then((res) => res.json()).then((res) => console.log(res)).catch((err) => {
     console.log(err);
   });
-*/ //  fetch(req).then((res) => res.json(console.log(res)));
-  console.log(filterCases);
-  console.log(filters);
-  console.log(systems);
+ //  fetch(req).then((res) => res.json(console.log(res)));
+  console.log(window.filterCases);
+  console.log(window.filters);
+  console.log(window.systems);
+  console.log(window.filterCases[0].product);
+          console.log(window.systems[i].attributes[j][2]);
+        console.log(window.systems[i].attributes[j][3]);
+        console.log(window.systems[i].attributes[j][4]);
+
+  */ //  fetch(req).then((res) => res.json(console.log(res)));
 
   const equipmentSelectionForm = document.querySelector('.equipment-selection__form');
   const waterPoints = equipmentSelectionForm.pa_water_points;
@@ -77,20 +83,60 @@ function main() {
     if (paClearTurbidityRadioBtn.value !== '' && choiceTasteRadioBtn.value !== '') {
       // нет ни одного такого, что пуст или не приобразуется в число
       valid = !Object.keys(allTableInputs).some(
-        (index) => Number.isNaN(Number(allTableInputs[index].value)) || allTableInputs[index].value === ''
+        (index) =>
+          Number.isNaN(Number(allTableInputs[index].value)) || allTableInputs[index].value === ''
       );
     } else valid = false;
-
     if (valid) {
       equipmentSelectionCalculateBtn.disabled = false;
     }
   }
 
+  function submitEquipmentSelectionForm(event) {
+    const dataForm = [];
+    console.log(dataForm);
+    dataForm.push({
+      name: paClearTurbidityRadioBtn[0].name,
+      value: paClearTurbidityRadioBtn.value,
+    });
+    dataForm.push({ name: choiceTasteRadioBtn[0].name, value: choiceTasteRadioBtn.value });
+    dataForm.push({ name: waterPoints.name, value: waterPoints.value });
+    Object.keys(allTableInputs).forEach((index) =>
+      dataForm.push({
+        name: allTableInputs[index].name,
+        value: allTableInputs[index].value,
+      })
+    );
+
+    /** проверяем все введеные в форму значения на соответствие диапазона каждого аттрибута
+     * каждой системы. Затем берем только те системы, у которых все атрибуты подходят
+     * под введеные значения, далее если есть системы то сортируем или ищем с наименьшей
+     * стоимостью, нет выводим сообщение */
+    for (let i = 0; i < window.systems.length; i += 1) {
+      for (let j = 2; j < window.systems[i].attributes.length; j += 1) {
+        for (let k = 2; k < dataForm.length; k += 1) {
+          if (dataForm[k].name === window.systems[i].attributes[j][2]) {
+            if (
+              Number(window.systems[i].attributes[j][3] <= Number(dataForm[k].value))
+              && Number(dataForm[k].value) <= Number(window.systems[i].attributes[j][4])
+            ) {
+              window.systems[i].attributes[j].push(true);
+            } else {
+              window.systems[i].attributes[j].push(false);
+            }
+          }
+        }
+      }
+    }
+    console.log(window.systems);
+    event.preventDefault();
+  }
   // бегунок
   waterPoints.addEventListener('change', changeWaterPoints);
   // ввод на форме
-
   equipmentSelectionForm.addEventListener('input', inputEquipmentSelectionForm);
+  // submit формы
+  equipmentSelectionForm.addEventListener('submit', submitEquipmentSelectionForm);
 }
 
 main();
