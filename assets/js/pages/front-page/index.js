438217d6582c6f06d5891ad98a96f4eec7ebb31f
Dmitry Lyapin <lyapindm@yandex.ru>
2020-06-01 14:59:49 +0300

    добавил поле имя

--- assets/js/pages/front-page/index.js
+++ assets/js/pages/front-page/index.js
@@ -2,13 +2,16 @@ function main() {
   const popUp = document.querySelector('.popup.popup-image');
   const promoBtn = document.querySelector('.promo__button');
   const popUpCloseBtn = document.querySelector('.popup__close');
+  const popupContentImg = popUp.querySelector('.popup__content_img');
 
   promoBtn.addEventListener('click', () => {
     popUp.classList.add('popup_is-opened');
+    popupContentImg.src = 'https://www.youtube.com/embed/LbLfrC-pDOc';
   });
 
   popUpCloseBtn.addEventListener('click', () => {
     popUp.classList.remove('popup_is-opened');
+    popupContentImg.src = '';
   });
 }
 
