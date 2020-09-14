237b29f0c04699b08755c09ee917e99b20e6541c
Dmitry Lyapin <lyapindm@yandex.ru>
2020-09-14 12:37:15 +0300

    сделал главную страницу

--- template-parts/front-page/front-page-templates/smart-phone-slider.php
+++ template-parts/front-page/front-page-templates/smart-phone-slider.php
@@ -1,28 +1,40 @@
 <section class="categories-slider">
   <h1 class="categories-slider__title" >Системы водоочистки, расходные материалы</h1>
   <div class="swiper-container categories-slider__container">
+    <div class="swiper-pagination categories-slider__pagination"></div>
     <div class="swiper-wrapper">
-      <div class="swiper-slide categories-slider__slide">
+      <a href="<?php echo SITE_URL?>sub-categories-systems" class="swiper-slide categories-slider__slide">
         <img class="categories-slider__pic" src="<?php echo SITE_URL?>wp-content/uploads/2020/03/ru_front_racurs_image_E4E5EB-84f5d.png" alt="Коплексные системы очистки воды">
          <div class="categories-slider__slide-wrapper">
-          <h4 class="categories-slider__title">Комплексные системы</h4>
+          <h4 class="categories-slider__sub-title">Комплексные системы</h4>
           <p class="categories-slider__text">Waterboss, Watermax, Aquaphor Pro и другие.</p>
           <p class="categories-slider__price">от 49 990 руб.</p>
          </div>
-      </div>
-
-
-      <div class="swiper-slide categories-slider__slide">
-        <img class="categories-slider__pic" src="<?php echo SITE_URL?>wp-content/uploads/2020/03/ru_front_racurs_image_ffffff-80f73.png" alt="Коплексны системы очистки воды">
-        <h4 class="categories-slider__title">Магистральные фильтры</h4>
-        <p class="categories-slider__text">Cepex&#174;, Гросс, Викинг для горячей или холодной воды.</p>
-        <p class="categories-slider__price">от 990 руб.</p>
-      </div>
-
-      <div class="swiper-slide categories-slider__slide">
-
-      </div>
+      </a>
+      <a href="<?php echo SITE_URL?>product-category/drinking" class="swiper-slide categories-slider__slide">
+        <img class="categories-slider__pic" src="<?php echo SITE_URL?>wp-content/uploads/2020/03/ru_front_racurs_image_ffffff-80f73.png" alt="Питьевые установки">
+         <div class="categories-slider__slide-wrapper">
+          <h4 class="categories-slider__sub-title">Питьевые установки</h4>
+          <p class="categories-slider__text">Обратный осмос и проточные фильтры под раковину</p>
+          <p class="categories-slider__price">от 3 100 руб.</p>
+         </div>
+      </a>
+      <a href="<?php echo SITE_URL?>product-category/trunk" class="swiper-slide categories-slider__slide">
+        <img class="categories-slider__pic" src="<?php echo SITE_URL?>wp-content/uploads/2020/03/ru_front_racurs_image_ffffff-33349.png" alt="Магистральные фильтр">
+         <div class="categories-slider__slide-wrapper">
+          <h4 class="categories-slider__sub-title">Магистральные фильтры</h4>
+          <p class="categories-slider__text">Cepex&#174;, Гросс, Викинг для горячей или холодной воды.</p>
+          <p class="categories-slider__price">от 990 руб.</p>
+         </div>
+      </a>
+      <a href="<?php echo SITE_URL?>product-category/расходники" class="swiper-slide categories-slider__slide">
+        <img class="categories-slider__pic" src="<?php echo SITE_URL?>wp-content/uploads/2020/05/ru_v150_plus_cartridge_page_image-772a3-1.png">
+         <div class="categories-slider__slide-wrapper">
+          <h4 class="categories-slider__sub-title">Расходные материалы</h4>
+          <p class="categories-slider__text">Сменные модули, картриджи, смола и соль для засыпки.</p>
+          <p class="categories-slider__price">от 105 руб.</p>
+         </div>
+      </a>
     </div>
-
-
+  </div>
 </section>
