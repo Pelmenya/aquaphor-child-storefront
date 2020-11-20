27c1b192ac84f3f57ebadda2c4e87936429549ad
Rodin Stasnislav <rodinstas@gmail.com>
2020-11-20 11:52:52 +0300

    Перенос изменений в основную ветку-фиксы замечаний от Димы

--- template-parts/header/header-smart-phone.php
+++ template-parts/header/header-smart-phone.php
@@ -34,9 +34,10 @@ function aquaphor_smart_phone_endpoint_title( $title, $id ) {
   elseif ( is_wc_endpoint_url( 'edit-account' ) ) {
       $title = "Ваш профиль";
   }
-  elseif ( is_wc_endpoint_url( 'order-received' ) ) {
-  $title = "Оформлен";
+  elseif ( is_order_received_page() ) {
+      $title = "Оформлен";
   }
+
   elseif ( is_view_order_page() ){
     // Вытаскиваем id заказа и его дату для хедера
     $url = $_SERVER['REQUEST_URI'];
@@ -59,31 +60,24 @@ add_filter( 'the_title', 'aquaphor_smart_phone_endpoint_title', 10, 2 );
 
 <div class="header header_smart-phone
   <?php
-    if (is_account_page()||is_cart()||is_checkout())
+    if (is_cart()||is_checkout())
       echo 'header_smart-phone_align';
-  ?>
-  <?php
-    if (is_front_page()){
-    if ($_GET["intro"] !== "false") echo 'header_smart-phone_is-closed';
-    }
-  ?>
-  ">
+  ?>">
   <?php
     if (is_product() ) {
       get_template_part('template-parts/header/header-smart-phone-templates/header__prev-page-product-button');
       get_template_part('template-parts/header/header-smart-phone-templates/header__search-and-logo');
     }
     else {
-      if (is_checkout()||is_account_page()||is_cart()){
+      if (is_checkout()||is_cart()||(is_page( 'delivery' ) )||(is_page( 'guarantees' ) )||(is_page( 'payment' ) )||(is_page( 'contacts' ) )||(is_page( 'about-company' ) )){
         set_header_prev_button(get_the_title(), "header__smart-phone-description-title_align-center");
       }
       else {
-          get_template_part('template-parts/header/header-smart-phone-templates/header__search-button');
-          get_template_part('template-parts/header/header-smart-phone-templates/header__search-and-logo');
+        get_template_part('template-parts/header/header-smart-phone-templates/header__search-button');
+        get_template_part('template-parts/header/header-smart-phone-templates/header__search-and-logo');
       }
     };
 
   ?>
 </div>
-
 <div class="buffer-for-sticky"></div>
